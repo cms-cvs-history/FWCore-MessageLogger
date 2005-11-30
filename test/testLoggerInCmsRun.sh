69ae8c3caca82cda4298fa014307cd46f610ba6c
Marc Paterno <sha1-50842b2973656903c5335439f1d47b932dc1ae12@cern.ch>
2005-11-30 20:02:46 +0000

    Fail test if cmsRun fails

--- test/testLoggerInCmsRun.sh
+++ test/testLoggerInCmsRun.sh
@@ -3,5 +3,4 @@
 # Pass in name and status
 function die { echo $1: status $2 ;  exit $2; }
 
-cmsRun --parameter-set ${LOCAL_TEST_DIR}/messageLogger.cfg
-
+cmsRun --parameter-set ${LOCAL_TEST_DIR}/messageLogger.cfg || die 'Failure using messageLogger.cfg' $?
