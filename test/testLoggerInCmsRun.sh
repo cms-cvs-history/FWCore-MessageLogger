b00c11336ff8cf0ba52702cb5c23bde3b53ca27e
Marc Paterno <sha1-50842b2973656903c5335439f1d47b932dc1ae12@cern.ch>
2005-12-06 17:44:21 +0000

    Fix command line that invokes cmsRun

--- test/testLoggerInCmsRun.sh
+++ test/testLoggerInCmsRun.sh
@@ -6,5 +6,5 @@ function die { echo $1: status $2 ;  exit $2; }
 export STD_OUT=${LOCAL_TMP_DIR}/cout.txt
 export STD_ERR=${LOCAL_TMP_DIR}/cerr.txt
 
-(cmsRun --parameter-set ${LOCAL_TEST_DIR}/messageLogger.cfg > ${STD_OUT} t 2> ${STD_ERR} ) || die 'Failure using messageLogger.cfg' $?
+(cmsRun --parameter-set ${LOCAL_TEST_DIR}/messageLogger.cfg > ${STD_OUT}  2> ${STD_ERR} ) || die 'Failure using messageLogger.cfg' $?
 
