cfd001ce415ede08ccee3d3d728cd1cf177f361f
Mark Steven Fischler <sha1-4e5f5390fe08571bcaa72b9888ceb66263060cc9@cern.ch>
2006-03-22 19:56:32 +0000

    test for behavior of overall category limit setting and fwk job report.

---
+++ test/UnitTestClient_D.cc
@@ -0,0 +1,34 @@
+#include "FWCore/MessageLogger/test/UnitTestClient_D.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/Handle.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+namespace edmtest
+{
+
+
+void
+  UnitTestClient_D::analyze( edm::Event      const & e
+                           , edm::EventSetup const & /*unused*/
+                              )
+{
+  edm::LogWarning("cat_A")   << "This message should not appear in " 
+  			     << "the framework job report";
+  edm::LogWarning("FwkJob")  << "This message should appear in " 
+ 			     << "the framework job report";
+  edm::LogWarning("special") << "This message should appear in " 
+ 			     << "restrict but the others should not";
+
+}  // MessageLoggerClient::analyze()
+
+
+}  // namespace edmtest
+
+
+using edmtest::UnitTestClient_D;
+DEFINE_FWK_MODULE(UnitTestClient_D)
